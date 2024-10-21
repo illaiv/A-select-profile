8432c3b9a54061929f41eb53149b126b743d030e
illaiv <48916017+illaiv@users.noreply.github.com>
2024-10-21 11:23:14 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,10 @@
+
+const setUp = () => {
+    $("#name").text("Vialli Yokhanis");
+    $("#major").text("CIT Computer Information & Technology ");
+    $("#year").text("Sophmore");
+    $("h4").html("<em>United States (family is from the Middle East)</em>")
+
+  };
+  
+  $(document).ready(setUp);
